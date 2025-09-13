30a8ea09f2db5425e51b93b6cd9c44cd599226d0
Luis Porras <lporras16@gmail.com>
2025-09-13 11:28:20 -0500

    removing node packages, update .gitignore, fix presentation on node 20

--- plugin/notes-server/index.js
+++ plugin/notes-server/index.js
@@ -1,60 +1,75 @@
 var express   = require('express');
 var fs        = require('fs');
+var http      = require('http');
+var path      = require('path');
 var io        = require('socket.io');
 var _         = require('underscore');
 var Mustache  = require('mustache');
 
-var app       = express.createServer();
+var app       = express();
+var server    = http.createServer(app);
 var staticDir = express.static;
 
-io            = io.listen(app);
+// Initialize socket.io with the http server
+io            = io(server);
 
-port = process.env.PORT || 3000;
+var port = process.env.PORT || 3000;
 
 var opts = {
-	port :      port,
-	baseDir :   __dirname + '/../../'
+    port:     port,
+    baseDir:  path.join(__dirname, '../../')
 };
 
-io.sockets.on('connection', function(socket) {
-	socket.on('slidechanged', function(slideData) {
-		socket.broadcast.emit('slidedata', slideData);
-	});
-	socket.on('fragmentchanged', function(fragmentData) {
-		socket.broadcast.emit('fragmentdata', fragmentData);
-	});
+// Set up socket.io connection
+io.on('connection', function(socket) {
+    socket.on('slidechanged', function(slideData) {
+        socket.broadcast.emit('slidedata', slideData);
+    });
+    socket.on('fragmentchanged', function(fragmentData) {
+        socket.broadcast.emit('fragmentdata', fragmentData);
+    });
 });
 
-app.configure(function() {
-	[ 'css', 'js', 'images', 'plugin', 'lib' ].forEach(function(dir) {
-		app.use('/' + dir, staticDir(opts.baseDir + dir));
-	});
+// Set up static file serving
+[ 'css', 'js', 'images', 'plugin', 'lib' ].forEach(function(dir) {
+    app.use('/' + dir, staticDir(path.join(opts.baseDir, dir)));
 });
 
+// Serve the main presentation
 app.get("/", function(req, res) {
-	fs.createReadStream(opts.baseDir + '/index.html').pipe(res);
+    fs.createReadStream(path.join(opts.baseDir, 'index.html')).pipe(res);
 });
 
+// Serve the notes page
 app.get("/notes/:socketId", function(req, res) {
-
-	fs.readFile(opts.baseDir + 'plugin/notes-server/notes.html', function(err, data) {
-		res.send(Mustache.to_html(data.toString(), {
-			socketId : req.params.socketId
-		}));
-	});
-	// fs.createReadStream(opts.baseDir + 'notes-server/notes.html').pipe(res);
+    fs.readFile(path.join(opts.baseDir, 'plugin/notes-server/notes.html'), function(err, data) {
+        if (err) {
+            console.error('Error reading notes.html:', err);
+            return res.status(500).send('Error loading notes');
+        }
+        res.send(Mustache.to_html(data.toString(), {
+            socketId: req.params.socketId
+        }));
+    });
 });
 
-// Actually listen
-app.listen(opts.port || null);
+// Error handling middleware
+app.use(function(err, req, res, next) {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
 
-var brown = '\033[33m',
-	green = '\033[32m',
-	reset = '\033[0m';
+// Start the server
+server.listen(opts.port || 3000, function() {
+    var brown = '\x1b[33m',
+        green = '\x1b[32m',
+        reset = '\x1b[0m';
 
-var slidesLocation = "http://localhost" + ( opts.port ? ( ':' + opts.port ) : '' );
+    var port = server.address().port;
+    var slidesLocation = "http://localhost:" + port;
 
-console.log( brown + "reveal.js - Speaker Notes" + reset );
-console.log( "1. Open the slides at " + green + slidesLocation + reset );
-console.log( "2. Click on the link your JS console to go to the notes page" );
-console.log( "3. Advance through your slides and your notes will advance automatically" );
+    console.log(brown + 'reveal.js - Speaker Notes' + reset);
+    console.log('1. Open the slides at ' + green + slidesLocation + reset);
+    console.log('2. Click on the link in your JS console to go to the notes page');
+    console.log('3. Advance through your slides and your notes will advance automatically');
+});
